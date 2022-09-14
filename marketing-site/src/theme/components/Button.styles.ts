f8d8db285f3b7b918333f04efb124c871e37c849
richgiang <hello@richardtongiang.com>
2022-09-14 15:31:21 -0400

    styling

---
+++ marketing-site/src/theme/components/Button.styles.ts
@@ -0,0 +1,33 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+export const ButtonStyles: ComponentStyleConfig = {
+  // style object for base or default style
+  baseStyle: {},
+  // styles for different sizes ("sm", "md", "lg")
+  sizes: {
+    regular: {
+      height: "3rem",
+      padding: "0.75rem 1rem",
+    },
+    small: {
+      height: "2rem",
+      padding: "0.5rem 0.75rem",
+    },
+  },
+  // styles for different visual variants ("outline", "solid")
+  variants: {
+    primary: {
+      bg: "none",
+      color: "white",
+      _hover: {
+        bg: "whtie",
+        color: "black",
+      },
+    },
+  },
+  // default values for 'size', 'variant' and 'colorScheme'
+  defaultProps: {
+    variant: "primary",
+    sizes: "regular",
+  },
+};
